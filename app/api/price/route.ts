3d1479882283bd7ad8a6cc2b1e65355d001cb09d
eyecand <eerokhov@inbox.ru>
2025-07-09 22:55:04 +1000

    add endpoint price new calculation

--- app/api/price/route.ts
+++ app/api/price/route.ts
@@ -1,5 +1,7 @@
 import { CustomsDuty } from "@/app/widjet/calculation-alert/lib/customs-duty";
+import { CalculationCar } from "@/lib/calcilation-car";
 import { CalculationUtilSbor } from "@/lib/calculation-util-sbor";
+import { DetectedCustomsClearance } from "@/lib/detected-customs-clearance";
 import { DetectedFullYear } from "@/lib/detected-full-year";
 import { DetectedKPower } from "@/lib/detected-K-power";
 import { prisma } from "@/prisma/prisma-client";
@@ -9,9 +11,19 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.searchParams.get("id") || null;
   const power = request.nextUrl.searchParams.get("power") || "1";
-  const response = await axios.get<CBRPRops>(
-    "https://www.cbr-xml-daily.ru/daily_json.js"
-  );
+
+  const cbr = await prisma.currency_rates.findMany({
+    select: {
+      char_code: true,
+      name: true,
+      value: true,
+    },
+  });
+  const EUR = cbr.find((item) => item.char_code === "EUR")?.value;
+  const KRW = cbr.find((item) => item.char_code === "KRW")?.value;
+  const fraht = cbr.find((item) => item.char_code === "fraht")?.value;
+  const broker = cbr.find((item) => item.char_code === "broker")?.value;
+  const k_krw = cbr.find((item) => item.char_code === "K_KRW")?.value;
 
   const car = await prisma.encar_vehicles.findFirst({
     where: {
@@ -30,55 +42,39 @@ export async function GET(request: NextRequest) {
       },
     },
   });
-  const differentYear = DetectedFullYear(String(car?.details?.release_date));
-  const util = CalculationUtilSbor(
-    Number(differentYear),
-    Number(car?.details?.engine_displacement)
-  );
-  //   const isProhodCarMainPage = NoProhodCar(
-  //     String(car?.details?.release_date)
-  //   );
   const K_Power = DetectedKPower(Number(power));
-  const customsCoast = CustomsDuty(
+  const oformlenie = DetectedCustomsClearance(
+    Number(car?.advertisements?.price) * Number(KRW) * 10
+  );
+  const poshlina = CustomsDuty(
     Number(car?.advertisements?.price) * 10000,
-    Number((Number(response.data.Valute.KRW.Value) * 1.08).toFixed(2)),
-    response.data.Valute.EUR.Value,
+    Number(KRW),
+    Number(EUR),
     Number(car?.details?.engine_displacement),
     String(car?.details?.fuel.fuel_english),
-    differentYear,
+    DetectedFullYear(String(car?.details?.release_date)),
     Number(power),
     K_Power
   );
-  const customs = 100000; //таможня
-
-  const fraht = 2100000;
-  const totalKorea = Math.floor(
-    Number(car?.advertisements?.price) * 10000 + fraht
+  const util = CalculationUtilSbor(
+    DetectedFullYear(String(car?.details?.release_date)),
+    Number(car?.details?.engine_displacement)
   );
-  const totalRussia = customsCoast + customs + util;
+  console.log("russia", poshlina + Number(broker) + util + oformlenie);
+  console.log(
+    "korea",
+    Number(car?.advertisements?.price) * 10000 + Number(fraht)
+  );
   const total =
     Math.floor(
-      totalKorea *
-        Number((Number(response.data.Valute.KRW.Value) * 1.08).toFixed(2)) *
-        0.001
-    ) + totalRussia;
+      (Number(car?.advertisements?.price) * 10000 + Number(fraht)) *
+        Number(KRW) *
+        0.001 *
+        Number(k_krw)
+    ) +
+    poshlina +
+    Number(broker) +
+    util +
+    oformlenie;
   return NextResponse.json(total);
-}
-interface CBRPRops {
-  Date: string; // Или Date, если будете парсить строку в Date объект
-  PreviousDate: string; // Или Date
-  PreviousURL: string;
-  Timestamp: string; // Или Date
-  Valute: {
-    [key: string]: ValuteData; // Индексный тип для динамических ключей валют (AUD, AZN, ...)
-  };
-}
-interface ValuteData {
-  ID: string;
-  NumCode: string;
-  CharCode: string;
-  Nominal: number;
-  Name: string;
-  Value: number;
-  Previous: number;
 }
